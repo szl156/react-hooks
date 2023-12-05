02868c3a623c2868cfabfb6d59c5f3a6d537ea0c
zlsheng <zlsheng@trip.com>
2023-12-05 10:59:58 +0800

    feat: 新增userarraytest演示页面

--- src/page/hookTest/UseArrayTest.tsx
+++ src/page/hookTest/UseArrayTest.tsx
@@ -6,10 +6,32 @@ type Props = {
 };
 
 const UseArrayTest: React.FC<Props> = ({ arr }) => {
+  const _arr = arr;
   const { array, set, push, filter, update, remove, clear } = useArray(arr);
+
+  const operateBtnList = [
+    { operate: "set", cb: () => set(_arr) },
+    {
+      operate: "push",
+      cb: () =>
+        push({
+          id: array[array.length - 1].id + 1,
+          name: `push${array[array.length - 1].id + 1}`,
+        }),
+    },
+    { operate: "filter", cb: () => filter((item) => item.id === 1) },
+    { operate: "update", cb: () => update(0, { id: 1, name: "update" }) },
+    { operate: "remove", cb: () => remove(0) },
+    { operate: "clear", cb: () => clear() },
+  ];
 
   return (
     <>
+      {operateBtnList.map((item, index) => (
+        <button key={index} onClick={() => item.cb()}>
+          {item.operate}
+        </button>
+      ))}
       <ul>
         {array.map((item, index) => (
           <li key={index}>
